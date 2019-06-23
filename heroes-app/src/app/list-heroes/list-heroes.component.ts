f44d7e5ed65a50f92b490b3fcaa00ec3fae85dc3
Andre Brito <dentis99@gmail.com>
2019-06-23 12:40:59 -0300

    Applied list hero layout

--- heroes-app/src/app/list-heroes/list-heroes.component.ts
+++ heroes-app/src/app/list-heroes/list-heroes.component.ts
@@ -1,12 +1,13 @@
 import { Component, OnInit } from '@angular/core';
 import { Hero } from '../hero';
+import { Power } from '../power';
 import {HeroesService} from "../Heroes/heroes.service";
 
 
 @Component({
   selector: 'app-list-heroes',
   templateUrl: './list-heroes.component.html',
-  styleUrls: ['./list-heroes.component.css']
+  styleUrls: ['../insert-hero/insert-hero.component.css']
 })
 export class ListHeroesComponent implements OnInit {
 
@@ -21,6 +22,19 @@ export class ListHeroesComponent implements OnInit {
         this.listHeroes = data;
       }
   );
+  }
+
+  getPower(powers:Power[]): String{
+    let firstPower = true;
+    let powerString = "";
+    powers.forEach(element => {
+      if(!firstPower) powerString += ", ";
+      else firstPower = false;
+      powerString += element.description;
+      
+    });
+    return powerString;
+  }
 
   }
 
